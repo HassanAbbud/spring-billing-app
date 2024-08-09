b2fdcd430a9e114dfca94beca9c6de2f28bed971
Hassan Abbud <hassanabbud@hotmail.com>
2024-08-09 11:16:48 -0600

    App models setup

--- src/main/java/com/hassan/springboot/di/bill/springboot_bill/models/Item.java
+++ src/main/java/com/hassan/springboot/di/bill/springboot_bill/models/Item.java
@@ -1,5 +1,20 @@
 package com.hassan.springboot.di.bill.springboot_bill.models;
 
 public class Item {
+    private Product product;
+    private Integer quantity;
+    
+    public Product getProduct() {
+        return product;
+    }
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+    public Integer getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 
 }
