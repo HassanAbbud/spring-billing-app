5d05b8d4d2416ea7bef744477a40e3b37f381cf0
Hassan Abbud <hassanabbud@hotmail.com>
2024-08-09 13:05:00 -0600

    Use other items for invoice

--- src/main/java/com/hassan/springboot/di/bill/springboot_bill/AppConfig.java
+++ src/main/java/com/hassan/springboot/di/bill/springboot_bill/AppConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 
 import com.hassan.springboot.di.bill.springboot_bill.models.Item;
@@ -19,6 +20,15 @@ public class AppConfig {
         Product p1 = new Product("iPhone", 1000);
         Product p2 = new Product("Google Pixel", 800);
         return Arrays.asList(new Item(p1,2), new Item(p2,4));
-        
+    }
+
+    @Bean
+    @Primary
+    List<Item> itemsInvoiceOffice(){
+        Product p1 = new Product("Monitor", 800);
+        Product p2 = new Product("Chair", 50);
+        Product p3 = new Product("Printer", 300);
+        Product p4 = new Product("Desk", 100);
+        return Arrays.asList(new Item(p1,5), new Item(p2,10),new Item(p3,2),new Item(p4,10));
     }
 }
